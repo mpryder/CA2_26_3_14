2087fd75d037eb90907ad9ed9ada372a05a8cb4b
Mark Ryder <ryddlemethis@gmail.com>
2014-03-26 20:52:25 +0000

    added event listners

--- js/main.js
+++ js/main.js
@@ -10,3 +10,58 @@ window._skel_config = {
         mobile: { range: '-480', containers: 'fluid', lockViewport: true, grid: { collapse: true } }
     }
 };
+
+$(document).ready(function() {
+    $("#addBn").click(function() {
+        var a = $("#input1").val();
+        var b = $("#input2").val();
+
+
+        if (((a.length == 0) && b.length == 0)) {
+           alert("Please enter a number");
+       }
+        $("#answer").val(Cal.add(a,b));
+
+    });
+    $("#subBn").click(function() {
+        var c = $("#input1").val();
+        var d = $("#input2").val();
+        if ((c.length == 0) && (d.length == 0)) {
+            alert("Please enter a number");
+        }
+
+        $("#answer").val(Cal.sub(c,d));
+
+    });
+    $("#multiplyBn").click(function() {
+        var e = $("#input1").val();
+        var f = $("#input2").val();
+
+        if (((e.length == 0) && f.length == 0)) {
+            alert("Please enter a number");
+        }
+
+        $("#answer").val(Cal.multiply(e,f));
+
+    });
+    $("#divideBn").click(function() {
+        var g = $("#input1").val();
+        var h = $("#input2").val();
+
+        if (((g.length == 0) && h.length == 0)) {
+            alert("Please enter a number");
+        }
+        if((g == 0) || (h === 0)){
+            alert("Cannot divide by 0");
+        }
+
+        $("#answer").val(Cal.divide(g,h));
+
+    });
+    $("#resetBn").click(function() {
+        $("#input1").val('');
+        $("#input2").val('');
+        $("#answer").val('');
+    });
+
+});
